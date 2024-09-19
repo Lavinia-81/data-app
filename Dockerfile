900cea5c40b0a3fb1d9fcad6a3b3946c3bf37888
Lavinia-81 <131885710+Lavinia-81@users.noreply.github.com>
2024-09-19 11:22:19 +0100

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,4 @@ RUN mkdir -p /home/app
 
 COPY . /home/app
 
-
 CMD ["node", "/home/app/server.js"]
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
@@ -17,11 +18,12 @@ app.get('/', function (req, res) {
 app.get('/get-profile', function (req, res) {
     var response = res;
 
-  MongoClient.connect('mongodb://admin:password@localhost:27017', function (err, client) {
+  MongoClient.connect('mongodb://admin:password@localhost:27017/user_profiles', function (err, client) {
      if (err) throw err;
 
     var db = client.db('user-account');
     var query = { userid: 1 };
+
     db.collection('users').findOne(query, function (err, result) {
       if (err) throw err;
       client.close();
@@ -39,14 +41,14 @@ app.post('/update-profile', function (req, res) {
   MongoClient.connect('mongodb://admin:password@localhost:27017', function (err, client) {
       if (err) throw err;
 
-    var db = client.db('user-account');
+    var db = client.db('my-db');
     userObj ['userid'] = 1
-    var query = { userid: 1 };
+    var myquery = { userid: 1 };
     var newValues = { $set: userObj };
 
     console.log('successfully connected to the user-admin db');
 
-    db.collection('users').updateOne(query, newValues, {upsert: true}, function (err, res) {
+    db.collection('users').updateOne(myquery, newValues, {upsert: true}, function (err, res) {
        if (err) throw err;
        console.log('successfully updated or inserted');
        client.close();
